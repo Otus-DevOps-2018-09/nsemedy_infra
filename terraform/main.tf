a3ce7a82b2bf51a7a23b5b37babb732a8f749d08
n.konyaeva <n.konyaeva@semrush.com>
2018-12-14 18:58:59 +0300

    start homework

---
+++ terraform/main.tf
@@ -0,0 +1,51 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+ resource "google_compute_project_metadata_item" "default" {
+  key   = "ssh-key"
+  value = "appuser:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
+}
+ resource "google_compute_instance" "app" {
+  name         = "${format("reddit-app%02d", count.index+1)}"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  count        = 2
+   boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+   network_interface {
+    network       = "default"
+    access_config = {}
+  }
+   metadata {
+    ssh-keys = "appuser:${file(var.private_key_path)}"
+  }
+   tags = ["reddit-app"]
+   connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+   provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+   provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+ resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+   allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+   source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
