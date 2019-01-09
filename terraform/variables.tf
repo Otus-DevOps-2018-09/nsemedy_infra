da6fcd52012eaf3d12c6ad5e9820f22841092fde
n.konyaeva <n.konyaeva@datatile.eu>
2019-01-10 02:04:15 +0300

    formatting

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,25 @@
 variable project {
   description = "Project ID"
 }
+
 variable region {
   description = "Region"
-  default = "europe-west1"
+  default     = "europe-west1"
 }
+
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
+
 variable private_key_path {
   description = "Path to the public key used for ssh access"
 }
+
 variable disk_image {
   description = "Disk image"
 }
 
 variable zone {
   description = "zone"
-  default = "europe-west1-b"
+  default     = "europe-west1-b"
 }
